5f98ccf2ea4fb6af05c855ece135e541d0c8eda0
Kevinpaulbabu1820 <kevinpaulbabu1820@gmail.com>
2023-04-09 10:49:13 +0530

    files added

---
+++ calculator-app/src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./Component/Calculator";
+import "./styles/calculator.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
